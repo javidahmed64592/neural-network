4a15cabe907bf9c289a4b4c17502f7b2e1110e39
Javid Ahmed <javidahmed@icloud.com>
2024-04-09 23:31:10 +0100

    Update neural network tests

--- tests/nn/test_neural_network.py
+++ tests/nn/test_neural_network.py
@@ -3,14 +3,29 @@ from src.nn.neural_network import NeuralNetwork
 
 class TestNeuralNetwork:
     def test_given_inputs_when_performing_feedforward_then_check_output_has_correct_shape(self):
-        test_inputs = [1, 0, 1]
+        test_inputs = [1.0, 0.0, 1.0]
         num_inputs = len(test_inputs)
         num_outputs = 4
         test_nn = NeuralNetwork(num_inputs, 5, num_outputs)
 
         output = test_nn.feedforward(test_inputs)
 
-        expected_shape = (num_outputs,)
-        actual_shape = output.shape
+        expected_len = num_outputs
+        actual_len = len(output)
 
-        assert actual_shape == expected_shape
+        assert actual_len == expected_len
+
+    def test_given_inputs_and_outputs_when_training_then_check_error_has_correct_shape(self):
+        test_inputs = [1.0, 0.0, 1.0]
+        num_inputs = len(test_inputs)
+
+        expected_outputs = [2, 3]
+        num_outputs = len(expected_outputs)
+        test_nn = NeuralNetwork(num_inputs, 5, num_outputs)
+
+        output = test_nn.train(test_inputs, expected_outputs)
+
+        expected_len = num_outputs
+        actual_len = len(output)
+
+        assert actual_len == expected_len
