07fa1131f94bbdd05c1f5c13363061c0bf09fa3b
Javid Ahmed <javidahmed@icloud.com>
2024-04-27 19:44:38 +0100

    Add tests to check adding and removing Nodes

--- tests/test_neural_network.py
+++ tests/test_neural_network.py
@@ -15,3 +15,19 @@ class TestNeuralNetwork:
         output = mock_nn.train(mock_inputs, mock_outputs)
         actual_len = len(output)
         assert actual_len == mock_len_outputs
+
+    def test_given_nn_when_adding_random_node_then_check_output_has_correct_shape(
+        self, mock_nn: NeuralNetwork, mock_inputs: list[float], mock_len_outputs: int
+    ) -> None:
+        mock_nn.mutate(1, 1, 0)
+        output = mock_nn.feedforward(mock_inputs)
+        actual_len = len(output)
+        assert actual_len == mock_len_outputs
+
+    def test_given_nn_when_removing_random_node_then_check_output_has_correct_shape(
+        self, mock_nn: NeuralNetwork, mock_inputs: list[float], mock_len_outputs: int
+    ) -> None:
+        mock_nn.mutate(1, 0, 1)
+        output = mock_nn.feedforward(mock_inputs)
+        actual_len = len(output)
+        assert actual_len == mock_len_outputs
