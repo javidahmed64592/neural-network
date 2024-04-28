e35fd0e3177b9f1edf5de4ad409fd5c6f34fbd2c
Javid Ahmed <javidahmed@icloud.com>
2024-04-28 01:25:47 +0100

    Add test for mixing matrices

--- tests/math/test_matrix.py
+++ tests/math/test_matrix.py
@@ -149,3 +149,16 @@ class TestMatrix:
         actual_shape = new_matrix.shape
         assert np.all(actual_shape == expected_shape)
         assert not np.all(matrix_1.vals == new_matrix.vals)
+
+    def test_given_matrices_when_mixing_then_check_output_has_correct_shape(self) -> None:
+        array_in_1 = np.array([[1, 2], [4, 3], [2, 4]])
+        array_in_2 = np.array([[-1, 1], [2, -5], [3, 2], [3, 1]])
+        array_out = np.array([[-1, 1, 2], [2, -5, 3], [3, 2, 1]])
+
+        matrix_in_1 = Matrix.from_array(array_in_1)
+        matrix_in_2 = Matrix.from_array(array_in_2)
+        matrix_out = Matrix.from_array(array_out)
+
+        new_matrix = Matrix.mix_matrices(matrix_in_1, matrix_in_2, matrix_out)
+
+        assert np.all(new_matrix.shape == matrix_out.shape)
