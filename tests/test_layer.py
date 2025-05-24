9d89baf9f8982d1d7f5daa6467290bdac2a65269
Javid Ahmed <javidahmed@icloud.com>
2025-05-24 01:03:31 +0100

    Increase code coverage

--- tests/test_layer.py
+++ tests/test_layer.py
@@ -1,4 +1,4 @@
-from neural_network.layer import HiddenLayer, InputLayer
+from neural_network.layer import HiddenLayer, InputLayer, OutputLayer
 from neural_network.math.matrix import Matrix
 
 
@@ -7,12 +7,16 @@ class TestLayer:
         self,
         mock_input_layer: InputLayer,
         mock_hidden_layer_1: HiddenLayer,
-        mock_len_hidden: list[int],
+        mock_output_layer: OutputLayer,
         mock_len_inputs: int,
+        mock_len_hidden: list[int],
+        mock_len_outputs: int,
     ) -> None:
         assert mock_input_layer.size == mock_len_inputs
         assert mock_hidden_layer_1.size == mock_len_hidden[0]
         assert mock_hidden_layer_1.num_inputs == mock_len_inputs
+        assert mock_output_layer.size == mock_len_outputs
+        assert mock_output_layer.num_inputs == mock_len_hidden[-1]
 
     def test_given_number_of_nodes_when_creating_layer_then_check_weights_and_bias_have_correct_shape(
         self, mock_hidden_layer_1: HiddenLayer, mock_len_hidden: list[int], mock_len_inputs: int
@@ -24,6 +28,13 @@ class TestLayer:
         actual_bias_shape = mock_hidden_layer_1.bias.shape
         assert actual_weights_shape == expected_weights_shape
         assert actual_bias_shape == expected_bias_shape
+
+    def test_given_weights_when_getting_random_weight_then_check_value_is_within_range(
+        self, mock_hidden_layer_1: HiddenLayer
+    ) -> None:
+        random_weight = mock_hidden_layer_1.random_weight
+        weight_range = mock_hidden_layer_1._weights_range
+        assert weight_range[0] <= random_weight <= weight_range[1]
 
     def test_given_inputs_when_performing_feedforward_then_check_output_has_correct_shape(
         self, mock_hidden_layer_1: HiddenLayer, mock_len_hidden: list[int], mock_input_matrix: Matrix
